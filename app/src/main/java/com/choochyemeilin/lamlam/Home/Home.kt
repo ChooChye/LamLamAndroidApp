298dc60091c0898153350f352bf4c7be0fec50ea
ChooChye <37932895+ChooChye@users.noreply.github.com>
2020-11-29 12:13:28 +0800

    Connection to MySQL added Retrieving of Data Added

--- app/src/main/java/com/choochyemeilin/lamlam/Home/Home.kt
+++ app/src/main/java/com/choochyemeilin/lamlam/Home/Home.kt
@@ -2,7 +2,6 @@ package com.choochyemeilin.lamlam.Home
 
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
 import android.view.MenuItem
@@ -11,6 +10,7 @@ import android.widget.AdapterView
 import android.widget.GridView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import com.choochyemeilin.lamlam.R
 import com.choochyemeilin.lamlam.Scan.Scan
 import com.choochyemeilin.lamlam.helpers.Lcg
@@ -21,7 +21,8 @@ import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.util.EntityUtils
-import java.lang.Exception
+import org.json.JSONArray
+import org.json.JSONObject
 
 class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
 
@@ -52,9 +53,21 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
 
         nav_view.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.mItem1 -> Toast.makeText(applicationContext, "Clicked Item 1", Toast.LENGTH_SHORT).show()
-                R.id.mItem2 -> Toast.makeText(applicationContext, "Clicked Item 2", Toast.LENGTH_SHORT).show()
-                R.id.mItem3 -> Toast.makeText(applicationContext, "Clicked Item 3", Toast.LENGTH_SHORT).show()
+                R.id.mItem1 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Item 1",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.mItem2 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Item 2",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.mItem3 -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Item 3",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
             true
         }
@@ -91,7 +104,7 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
         arrayList.add(HomeItem(R.drawable.qr_code, "SCAN"))
         arrayList.add(HomeItem(R.drawable.magnifier, "SEARCH"))
         arrayList.add(HomeItem(R.drawable.loan, "LOANS"))
-        arrayList.add(HomeItem(R.drawable.business_report , "REPORTS"))
+        arrayList.add(HomeItem(R.drawable.business_report, "REPORTS"))
         arrayList.add(HomeItem(R.drawable.exchange, "RETURN ITEMS"))
 
         return arrayList
@@ -107,7 +120,7 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
             1 -> {
                 Toast.makeText(applicationContext, "SEARCH", Toast.LENGTH_SHORT).show()
             }
-            2 ->{
+            2 -> {
                 Toast.makeText(applicationContext, "LOANS", Toast.LENGTH_SHORT).show()
             }
             3 -> {
@@ -126,24 +139,39 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
 
     private fun httpRun(){
 
-        val url = "http://10.0.2.2/www/FYP/WebApp/android/connect.php"
-        //val url = "http://192.168.1.138/www/FYP/WebApp/android/connect.php"
-        //val url = "http://10.0.2.16:8080/www/FYP/WebApp/android/connect.php"
-        //val url = "http://127.0.0.1:80/www/FYP/WebApp/android/connect.php"
-        //val url = "https://google.com"
+        val url = "http://10.0.2.2/www/FYP/WebApp/android/connect.php" //change when going live
 
+        var id : String ?= null
+        var username : String ?= null
 
+        try{
+            val jsonObject: JSONObject = getJsonData("connect.php")
+            id = jsonObject.getString("userID")
+            username = jsonObject.getString("userName")
+
+            utils.log("Test = passed. Connected to MySQL | ${id}  ${username}")
+        }catch (e: Exception){
+            utils.log("An error has occurred Error #1654")
+        }
+    }
+
+    private fun getJsonData(phpFile : String) : JSONObject{
+        val url = "http://10.0.2.2/www/FYP/WebApp/android/$phpFile"
+        var jsonObject: JSONObject ?=null
         try{
             val httpClient : HttpClient = DefaultHttpClient()
             val httpPost : HttpPost = HttpPost(url)
             val httpResp : HttpResponse = httpClient.execute(httpPost)
             val respBody = EntityUtils.toString(httpResp.entity)
-            utils.log("Test = passed. Connected to MySQL | ${respBody}")
-            //Toast.makeText(applicationContext, "MySQL connecting...", Toast.LENGTH_SHORT).show()
-        }catch (e: Exception){
-            utils.log("Error connecting to MySQL | ${e.message}")
-            //Toast.makeText(applicationContext, "Error connecting to MySQL", Toast.LENGTH_LONG).show()
-        }
-    }
 
+            val parseJson : JSONArray = JSONArray(respBody)
+
+            for (i in 0 until parseJson.length()) {
+                jsonObject = parseJson.getJSONObject(i)
+            }
+        }catch (e: Exception){
+            utils.log("error getting json Error #1532")
+        }
+        return jsonObject!!
+    }
 }
