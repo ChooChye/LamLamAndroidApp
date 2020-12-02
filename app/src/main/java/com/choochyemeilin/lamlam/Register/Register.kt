fb53a2d753a1f25dd104d636fc9d79e53dc4249e
meilin0438 <chinmeilin0438@gmail.com>
2020-12-02 21:36:33 +0800

    2/12/2020 login ui, register ui

---
+++ app/src/main/java/com/choochyemeilin/lamlam/Register/Register.kt
@@ -0,0 +1,12 @@
+package com.choochyemeilin.lamlam.Register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.choochyemeilin.lamlam.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
