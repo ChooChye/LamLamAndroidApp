9085ce82905e615aafc7592ecd2695dae5c79025
meilin0438 <chinmeilin0438@gmail.com>
2020-12-05 23:50:20 +0800

    5/12/2020 nav

--- app/src/main/java/com/choochyemeilin/lamlam/Home/Home.kt
+++ app/src/main/java/com/choochyemeilin/lamlam/Home/Home.kt
@@ -59,14 +59,22 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
                     "Clicked Item 2",
                     Toast.LENGTH_SHORT
                 ).show()
+
+                /*
                 R.id.mItem3 -> Toast.makeText(
                     applicationContext,
                     "Clicked Item 3",
                     Toast.LENGTH_SHORT
                 ).show()
+
+                 */
+
+
             }
             true
         }
+
+
 
         gridView = findViewById(R.id.homeGridLayout)
         arrayList = ArrayList()
