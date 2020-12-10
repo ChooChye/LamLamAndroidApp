501c0b84a3b5efc69dd5330cdebf810436da21a4
ChooChye <37932895+ChooChye@users.noreply.github.com>
2020-12-10 11:32:40 +0800

    Scanning / Read JSON completed 10/12/20

---
+++ app/src/main/java/com/choochyemeilin/lamlam/Scan/ScanHistory.kt
@@ -0,0 +1,25 @@
+package com.choochyemeilin.lamlam.Scan
+
+class ScanHistory{
+    private var id : String ?= null
+    private var Category : String ?= null
+    private var Product : String ?= null
+
+
+    override fun toString(): String {
+        return "$id | $Category | $Product"
+    }
+
+    fun getID(): String{
+        return "$id"
+    }
+    fun getCategory(): String{
+        return "$Category"
+    }
+    fun getProduct(): String{
+        return "$Product"
+    }
+
+
+
+}
