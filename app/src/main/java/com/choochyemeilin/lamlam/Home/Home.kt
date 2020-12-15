6cf0b6e449bdb9ab94f7699f8a2ccd205e7859e0
ChooChye <37932895+ChooChye@users.noreply.github.com>
2020-12-15 15:58:19 +0800

    15/12/20 Bug Fixes

--- app/src/main/java/com/choochyemeilin/lamlam/Home/Home.kt
+++ app/src/main/java/com/choochyemeilin/lamlam/Home/Home.kt
@@ -1,10 +1,10 @@
 package com.choochyemeilin.lamlam.Home
 
 
-import android.app.Activity
+//import com.choochyemeilin.lamlam.Search.Search
+
 import android.content.Intent
 import android.os.Bundle
-import android.os.StrictMode
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
@@ -12,25 +12,17 @@ import android.widget.GridView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.FragmentNavigator
-import androidx.navigation.fragment.NavHostFragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.choochyemeilin.lamlam.Login.Login
 import com.choochyemeilin.lamlam.R
-import com.choochyemeilin.lamlam.Register.Register
 import com.choochyemeilin.lamlam.ReturnItems.ReturnItems
 import com.choochyemeilin.lamlam.Scan.Scan
-//import com.choochyemeilin.lamlam.Search.Search
+import com.choochyemeilin.lamlam.Scan.ScanHistory
 import com.choochyemeilin.lamlam.helpers.Lcg
 import com.choochyemeilin.lamlam.helpers.Utils
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_home.*
 
-import org.json.JSONArray
-import org.json.JSONObject
 
 class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
 
@@ -84,9 +76,7 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
         }
 
 
-        fun jsonTest(){
-            ArrayList<String>()
-        }
+
 
         //logout
         nav_view.menu.findItem(R.id.mItem4).setOnMenuItemClickListener {
@@ -116,8 +106,9 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
         val binary = chars[0].toInt()
         val finalBinary = String.format("%8s", Integer.toBinaryString(binary)).replace(' ', '0')
         welcome.text = lcg.toBinary(chars).toString()*/
-    }
 
+
+    }
     //Logout Methods
     private fun logout(){
 
@@ -162,9 +153,9 @@ class Home : AppCompatActivity(), AdapterView.OnItemClickListener {
                 Toast.makeText(applicationContext, "REPORTS", Toast.LENGTH_SHORT).show()
             }
             4 -> {
-               val intent = Intent(this, ReturnItems::class.java)
+                val intent = Intent(this, ReturnItems::class.java)
                 startActivity(intent)
-               // Toast.makeText(applicationContext, "RETURN ITEMS", Toast.LENGTH_SHORT).show()
+                // Toast.makeText(applicationContext, "RETURN ITEMS", Toast.LENGTH_SHORT).show()
             }
         }
     }
